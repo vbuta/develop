8d28a8a42c6506efd18df1a34a286ab92e45dd59
vbuta <53211629+vbuta@users.noreply.github.com>
2025-12-11 15:28:09 +0100

    Add files via upload

---
+++ dotnet/Models/VersionInfo.cs
@@ -0,0 +1,8 @@
+namespace dotnet.Models;
+
+public class VersionInfo
+{
+    public string DotnetRuntime { get; set; } = "";
+    public string OS { get; set; } = "";
+    public string Framework { get; set; } = "";
+}
